4711353c68b771f6cb4df0a3f81b507e4a00b641
Bengsiswanto <mightiesthero@gmail.com>
2019-02-06 22:11:01 +0700

    add navbar

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -5,18 +5,25 @@ import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 
+import MainNavigation from "./components/Navigation/MainNavigation";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/bookings" component={BookingsPage} />
-          <Route path="/events" component={EventsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/bookings" component={BookingsPage} />
+              <Route path="/events" component={EventsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
