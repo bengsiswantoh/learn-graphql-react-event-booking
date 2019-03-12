dccc7a85c64d9fe2f3db6196e23847a3991f722c
Bengsiswanto <mightiesthero@gmail.com>
2019-03-12 20:16:18 +0700

    sort events by id

--- backend-express/graphql/resolvers/merge.js
+++ backend-express/graphql/resolvers/merge.js
@@ -15,6 +15,7 @@ const userLoader = new DataLoader(userIds => {
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a,b) => eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString()) )
     return events.map(event => {
       return transformEvent(event);
     });
